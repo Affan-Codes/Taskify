24b57831874b5d275f319401c5abe5e58d233fbb
Affan-Codes <affan.khan4cs@gmail.com>
2025-11-10 18:03:39 +0530

    implement models and related funtionalities

---
+++ backend/src/models/rolePermission.model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Document, Schema } from "mongoose";
+import {
+  Permissions,
+  PermissionType,
+  Roles,
+  RoleType,
+} from "../enums/role.enum";
+import { RolePermissions } from "../utils/rolePermission";
+
+export interface RoleDocument extends Document {
+  name: RoleType;
+  permissions: Array<PermissionType>;
+}
+
+const roleSchema = new Schema<RoleDocument>(
+  {
+    name: {
+      type: String,
+      enum: Object.values(Roles),
+      required: true,
+      unique: true,
+    },
+    permissions: {
+      type: [String],
+      enum: Object.values(Permissions),
+      required: true,
+      default: function (this: RoleDocument) {
+        return RolePermissions[this.name];
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+export const RoleModel = mongoose.model<RoleDocument>("Role", roleSchema);
